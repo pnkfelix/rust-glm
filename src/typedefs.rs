17c4e4de2cc59b174ef644a3beeb1eca38365933
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-06-03 15:23:06 +0200

    add copyright info to rust-glm.

--- src/typedefs.rs
+++ src/typedefs.rs
@@ -1,3 +1,12 @@
+// Copyright 2014 Felix S. Klock II. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 #![allow(non_camel_case_types)]
 
 use super::vector::{TVec1, TVec2, TVec3, TVec4};
